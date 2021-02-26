48e95bb3f3dcad32d99c5507bf41eed13170faca
juchilian <juchilian@outlook.jp>
2021-02-26 14:14:55 +0900

    new ec2 ALLOWED_HOSTS

--- blog_app/settings.py
+++ blog_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '78jq3r%eosrwfqq@ys=qfjh%qf95(b1j_y#@(%g3$_v$rf==s*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '3.112.13.51']
+ALLOWED_HOSTS = ['127.0.0.1', '18.181.192.92', 'ec2-18-181-192-92.ap-northeast-1.compute.amazonaws.com']
 
 LOGIN_URL = '/login'
 
