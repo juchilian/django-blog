55d0e72e126d03ba0f002a94ab80975dd8e9864c
juchilian <juchilian@outlook.jp>
2021-03-01 08:56:13 +0900

    [add] banner animation

--- core/models.py
+++ core/models.py
@@ -6,6 +6,7 @@ from allauth.account.signals import user_logged_in
 User = get_user_model()
 
 def user_logged_in_receiver(request, user, *args, **kwargs):
-    print(request, user)
+    # print(request, user)
+    pass
 
 user_logged_in.connect(user_logged_in_receiver, sender=User)
